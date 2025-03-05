0502bf3269f358f49f1d164ac6f222006dc8b794
Shweta-Prasad <pshweta923@gmail.com>
2025-03-05 21:12:51 +0530

    Initial Commit

--- frontend/recipe-blog/src/App.jsx
+++ frontend/recipe-blog/src/App.jsx
@@ -11,7 +11,7 @@ import RecipeDetails from './pages/RecipeDetails';
 // Fetch all recipes
 const getAllRecipes = async() => {
   let allRecipes = []
-  await axios.get('https://recipe-blog-backend.vercel.app/recipe').then(res => {
+  await axios.get('http://localhost:5000/recipe').then(res => {
     allRecipes = res.data
   })
   return allRecipes
@@ -32,10 +32,10 @@ const getFavRecipes = async() => {
 // Fetch a single recipe with user details
 const getRecipe=async({params})=>{
   let recipe;
-  await axios.get(`https://recipe-blog-backend.vercel.app/recipe/${params.id}`)
+  await axios.get(`http://localhost:5000/recipe/${params.id}`)
   .then(res=>recipe=res.data)
 
-  await axios.get(`https://recipe-blog-backend.vercel.app/user/${recipe.createdBy}`)
+  await axios.get(`http://localhost:5000/user/${recipe.createdBy}`)
   .then(res=>{
     recipe={...recipe,email:res.data.email}
   })
